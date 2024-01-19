5d2a7e2f60bdee45bddeab35d74e60c4dc6b2a5c
OND <149911739+ONDM@users.noreply.github.com>
2024-01-19 21:48:52 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+  "name": "OND",
+  "short_name": "OND",
+  "description": "OND ALL APPS",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons":
+  [
+    {
+      "src": "favicon.png",
+      "sizes": "150x150",
+      "type": "image/png"
+    }
+  ]
+}
