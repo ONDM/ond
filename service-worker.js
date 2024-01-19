66f1babbfc672ddd2cdc09358f6895f4cd03df4d
OND <149911739+ONDM@users.noreply.github.com>
2024-01-19 23:58:42 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
 const cacheName = 'OND-cache-v2';
 
-self.addEventListener('install', (event) =>
-{
+self.addEventListener('install', (event) => {
   console.log('Service Worker Installed');
   event.waitUntil(
-    caches.open(cacheName).then((cache) =>
-    {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll([
         '/',
         '/ond/manifest.json',
@@ -20,12 +18,16 @@ self.addEventListener('install', (event) =>
   );
 });
 
-self.addEventListener('fetch', (event) =>
-{
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-  caches.match(event.request).then((response) =>
-  {
-    return response || fetch(event.request);
-  })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch((error) => {
+      console.error('Error fetching:', error);
+    })
   );
+});
+
+self.addEventListener('error', (error) => {
+  console.error('Service Worker Error:', error);
 });
