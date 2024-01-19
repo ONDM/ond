6d3f62ad8d0f77df69d7b8e985e3fbc7fa232ad5
OND <149911739+ONDM@users.noreply.github.com>
2024-01-19 21:48:39 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const cacheName = 'OND-cache-v2';
+
+self.addEventListener('install', (event) =>
+{
+  console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open(cacheName).then((cache) =>
+    {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        'style.css',
+        'script.js',
+        'favicon.png',
+        'font/font1.woff2',
+        'font/font2.woff2',
+        'sw.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) =>
+{
+  event.respondWith(
+  caches.match(event.request).then((response) =>
+  {
+    return response || fetch(event.request);
+  })
+  );
+});
